64e41753dbeb47a7aee533acdae81b48e91d18ff
rotem2022 <rotemchaya@gmail.com>
2022-04-13 16:34:56 +0300

    hi

--- AlgoEX1/UnionFind.h
+++ AlgoEX1/UnionFind.h
@@ -1,5 +1,14 @@
 #pragma once
+#include "Vertex.h"
+#include "MyMap.h"
 class UnionFind
 {
+
+  MyMap*  mapArray;
+   int size;
+   UnionFind(int size);
+   void makeSet(Vertex* v);
+   Vertex*  find(Vertex* v);
+   void  Union(Vertex* v, Vertex* u);
 };
 
